428b79aa621f8abde3af2f2f78e3fed76c98aa93
changlin0501 <guanguan19970501@gmail.com>
2019-03-28 09:17:28 +0800

    机器注册码
    *这是机器注册码

---
+++ MES-MonitoringClient/JiQi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace MES_MonitoringClient
+{
+	public partial class JiQi : Form
+	{
+		/*private Point mPoint;*///字段名
+
+		public JiQi()
+		{
+			InitializeComponent();
+		}
+		/// <summary>
+		/// 关闭按钮
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button2_Click(object sender, EventArgs e)
+		{
+			this.Close();
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+		
+		}
+	}
+}
+
