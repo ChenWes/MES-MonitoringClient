97e8868784b676d9e55968dac2ea049752c0d7d7
caichunwang <962548139@qq.com>
2019-11-14 15:51:30 +0800

    * 完成服务守护进程开发

---
+++ MES-Service-Defend/Common/LogHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace MES_Service_Defend.Common
+{
+    /// <summary>
+    /// 日志帮助类
+    /// </summary>
+    public static class LogHandler
+    {
+        public static readonly log4net.ILog loginfo = log4net.LogManager.GetLogger("loginfo");
+        public static readonly log4net.ILog logerror = log4net.LogManager.GetLogger("logerror");
+
+        /// <summary>
+        /// 写入日志（无异常）
+        /// </summary>
+        /// <param name="info">日志信息</param>
+        public static void WriteLog(string info)
+        {
+            if (loginfo.IsInfoEnabled)
+            {
+                loginfo.Info(info);
+            }
+        }
+
+        /// <summary>
+        /// 写入日志（有异常）
+        /// </summary>
+        /// <param name="info">日志信息</param>
+        /// <param name="ex">异常实体</param>
+        public static void WriteLog(string info, Exception ex)
+        {
+            if (logerror.IsErrorEnabled)
+            {
+                logerror.Error(info, ex);
+            }
+        }
+    }
+}
---
+++ MES-Service-Defend/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace MES_Service_Defend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //服务的默认值
+            string defaultServiceName = ConfigurationManager.AppSettings["DefaultServiceName"].ToString();
+            string defaultServiceDisplayName = ConfigurationManager.AppSettings["DefaultServiceDisplayName"].ToString();
+            string defaultServiceDescription = ConfigurationManager.AppSettings["DefaultServiceDescription"].ToString();
+
+            //后台测试方法
+            //BackendServiceHandler BackendServiceHandlerClass = new BackendServiceHandler();
+            //BackendServiceHandlerClass.ProcessMachineStatusLog();
+            //BackendServiceHandlerClass.ProcessSyncDataAction();
+            //BackendServiceHandlerClass.CheckMachineRegister();
+            //BackendServiceHandlerClass.ProcessEmployeeImage();
+            //BackendServiceHandlerClass.ProcessJobOrder();
+
+            var exitCode = HostFactory.Run(x =>
+            {
+                //配置服务
+                x.Service<BackendServiceHandler>(s =>
+                {
+                    //配置服务运行方法
+                    s.ConstructUsing(uploadstatus => new BackendServiceHandler());
+
+                    //配置服务开始与结束
+                    s.WhenStarted(uploadstatus => uploadstatus.Start());
+                    s.WhenStopped(uploadstatus => uploadstatus.Stop());
+                });
+
+                //运行在本地系统
+                x.RunAsLocalSystem();
+
+                //服务设置属性
+                x.SetServiceName(defaultServiceName);
+                x.SetDisplayName(defaultServiceDisplayName);
+                x.SetDescription(defaultServiceDescription);
+            });
+
+            //转换并返回退出编码
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
